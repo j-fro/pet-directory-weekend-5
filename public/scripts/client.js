d5e17e538283eb5c5a5ef20d52f0297ee03c3a9f
Jacob Froman <jacob.h.froman@gmail.com>
2017-01-07 15:05:35 -0600

    added pet input and save to db

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,5 +1,17 @@
 var petsApp = angular.module('petsApp', []);
 
-petsApp.controller('HomeController', ['$scope', function($scope) {
-    console.log('ng');
+petsApp.controller('HomeController', ['$scope', '$http', function($scope, $http) {
+    $scope.addPet = function() {
+        var newPet = {
+            name: $scope.nameIn,
+            animal: $scope.animalIn,
+            age: $scope.ageIn,
+            imgUrl: $scope.imgUrlIn
+        };
+        console.log('Adding pet:', newPet);
+        $http.post('/pets', newPet)
+            .then(function(results) {
+                console.log('Back from server:', results);
+            });
+    };
 }]);
