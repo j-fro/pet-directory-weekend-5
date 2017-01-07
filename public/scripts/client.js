442770e48362d20c3ce49688afa1e0bca19d9166
Jacob Froman <jacob.h.froman@gmail.com>
2017-01-07 15:57:53 -0600

    created ng routes

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,4 +1,44 @@
-var petsApp = angular.module('petsApp', []);
+var petsApp = angular.module('petsApp', ['ngRoute']);
+
+petsApp.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+        .when('/home', {
+            templateUrl: 'views/routes/home.html',
+            controller: 'HomeController'
+        })
+        .when('/add', {
+            templateUrl: 'views/routes/add.html',
+            controller: 'AddPetsController'
+        })
+        .when('/show', {
+            templateUrl: 'views/routes/show.html',
+            controller: 'ShowPetsController'
+        })
+        .otherwise({
+            redirectTo: 'home'
+        });
+}]);
+
+petsApp.controller('ShowPetsController', ['$scope', '$http', function($scope, $http) {
+    $scope.getPets = function() {
+        $http.get('/pets')
+            .then(function(results) {
+                console.log('Back from server:', results);
+                $scope.pets = results.data;
+            });
+    };
+    
+    $scope.deletePet = function(petId) {
+        $http.delete('/pets/' + petId)
+            .then(function(results) {
+                console.log('Back from server', results);
+                $scope.getPets();
+            });
+    };
+
+    // Perform initial getPets() on page load
+    $scope.getPets();
+}]);
 
 petsApp.controller('HomeController', ['$scope', '$http', function($scope, $http) {
     $scope.addPet = function() {
@@ -16,22 +56,5 @@ petsApp.controller('HomeController', ['$scope', '$http', function($scope, $http)
             });
     };
 
-    $scope.getPets = function() {
-        $http.get('/pets')
-            .then(function(results) {
-                console.log('Back from server:', results);
-                $scope.pets = results.data;
-            });
-    };
 
-    $scope.deletePet = function(petId) {
-        $http.delete('/pets/' + petId)
-            .then(function(results) {
-                console.log('Back from server', results);
-                $scope.getPets();
-            });
-    };
-
-    // Perform initial getPets() on page load
-    $scope.getPets();
 }]);
