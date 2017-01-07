4c37ff54ca1a6408d83e66b4212fa21670dea99b
Jacob Froman <jacob.h.froman@gmail.com>
2017-01-07 14:21:14 -0600

    spun up server and added npm start script

--- server/app.js
+++ server/app.js
@@ -2,3 +2,10 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 var mongoose = require('mongoose');
+var app = express();
+
+app.set('port', (process.env.PORT || 8000));
+
+app.listen(app.get('port'), function() {
+    console.log('Server listening on port', app.get('port'));
+});
